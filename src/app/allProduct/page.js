7e1ffca096bcee0e2da82e66b94860ec4865cd31
Melika Piri <me.piri@tosantechno.net>
2025-10-08 15:56:04 +0330

    chore: minor route adjustment in allProduct

--- src/app/allProduct/page.js
+++ src/app/allProduct/page.js
@@ -1,7 +1,9 @@
-import React,from 'react';
+import React, from 'react';
 import ProductWrapper from "@/src/components/Templates/Products/ProductWrapper";
 
 async function Page(props) {
+
+
     return (
         <div className="  my-10 ">
             <ProductWrapper/>
--- src/components/Templates/Products/ProductWrapper.js
+++ src/components/Templates/Products/ProductWrapper.js
@@ -1,16 +1,19 @@
 "use client";
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
+import { usePathname } from 'next/navigation'
 import FilterSideBarProduct from "@/src/components/Templates/Products/FilterSideBarProduct";
 import SortIcon from "@/src/components/Icons/SortIcon";
 import OfferCard from "@/src/components/modules/OfferCard/OfferCard";
 import SkeletonCard from "@/src/components/modules/SkeletonCard/SkeletonCard";
 import PaginationComponent from "@/src/components/modules/PaginationComponent/PaginationComponent";
-import {useFilterProduct} from "@/src/Context/FilterProductContext";
+import { useFilterProduct } from "@/src/Context/FilterProductContext";
 import SortSection from "@/src/components/Templates/Products/SortSection";
 
 function ProductWrapper() {
+    const pathname = usePathname()
+    console.log(pathname)
     const {
-        allProduct,              //Filtered list
+        allProduct,              // Filtered list
         paginatedProducts,       // Paginated list (final for display)
         setPaginatedProducts,
         originalProducts,
@@ -31,30 +34,30 @@ function ProductWrapper() {
     return (
         <>
             <div className="flex items-start flex-col lg:flex-row container xl:max-w-[94.52%] mx-auto gap-4 lg:gap-6">
-                <FilterSideBarProduct categories={allCategories}/>
+                <FilterSideBarProduct categories={allCategories} />
 
                 <section className="flex flex-col w-full gap-4 lg:gap-6">
                     <div
                         className="relative hidden md:flex items-center justify-between gap-x-6 h-14 px-8 bg-lightGray3 rounded-2xl">
                         <div className="flex items-center gap-x-2">
-              <span className="flex items-center gap-2 font-Yekan-Medium text-base">
-                <SortIcon className="w-5 h-5"/> مرتب سازی
-              </span>
+                            <span className="flex items-center gap-2 font-Yekan-Medium text-base">
+                                <SortIcon className="w-5 h-5" /> مرتب سازی
+                            </span>
                         </div>
-                        <SortSection/>
+                        <SortSection />
                         <span className="hidden xl:block font-Yekan-Medium text-sm text-[#404040]">
-              {originalProducts.length} محصول موجود است
-            </span>
+                            {originalProducts.length} محصول موجود است
+                        </span>
                     </div>
 
                     <div className="grid gap-4 sm:gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
                         {loading
                             ? paginatedProducts.map((product) => (
-                                <OfferCard products={product} key={product.id}/>
+                                <OfferCard products={product} key={product.id} />
                             ))
                             : Array(8)
                                 .fill(null)
-                                .map((_, i) => <SkeletonCard key={i}/>)}
+                                .map((_, i) => <SkeletonCard key={i} />)}
                     </div>
                 </section>
             </div>
