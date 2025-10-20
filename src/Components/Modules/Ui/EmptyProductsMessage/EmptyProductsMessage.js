d6969cce2767d6d787085ccb6d1357ff425eba0b
Melika Piri <me.piri@tosantechno.net>
2025-10-20 16:02:08 +0330

    fix bug EmptyProductsMessage component

--- src/Components/Modules/Ui/EmptyProductsMessage/EmptyProductsMessage.js
+++ src/Components/Modules/Ui/EmptyProductsMessage/EmptyProductsMessage.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function EmptyProductsMessage({message}) {
     return (
-        <div
-            className="text-center   max-w-xl mx-auto bg-primaryColor/80 text-secondPrimaryColor  rounded-2xl py-5 px-5 sm:px-20 m-5">{message}</div>
+        <div className="text-center max-w-xl mx-auto bg-primaryColor/80
+         text-secondPrimaryColor  rounded-2xl py-5 px-5 sm:px-20 m-5">{message}</div>
     );
 }
 
